ef180dc1880278d31fa1660037d97e2934fa12c3
unknown <shirefmessi@gmail.com>
2024-12-26 21:56:18 +0200

    enhance the scroll section and add overflow-hidden for it

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,56 +1,55 @@
-import CustomCard from './CustomCard';
-import vegSandwich from '../assets/01.jpg';
-import burgerImg from '../assets/02.jpg';
-import pizzaImg from '../assets/03.jpg';
+import CustomCard from "./CustomCard";
+import vegSandwich from "../assets/01.jpg";
+import burgerImg from "../assets/02.jpg";
+import pizzaImg from "../assets/03.jpg";
 
 const Explore = () => {
-    return (
-        <section className="explore" id="explore">
-            <div className="container">
-                <h4 className="fw-bold text-center" data-aos="fade-right">
-                    Explore Our Foods
-                </h4>
-                <p className="text-center my-4" data-aos="fade-left">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Etiam et purus a odio finibus bibendum in sit amet leo.
-                    Mauris feugiat erat tellus. Far far away, behind the word
-                    mountains, far from the countries Vokalia and Consonantia,
-                    there live the blind texts. Separated they live in
-                    Bookmarksgrove.
-                </p>
-                <div
-                    className="row justify-content-between"
-                    data-aos="fade-up"
-                    data-aos-delay="200"
-                >
-                    <CustomCard
-                        image={vegSandwich}
-                        altImg="vegetable-sandwich"
-                        description="Rainbow Vegetable Sandwich"
-                        time="15 - 20 Minutes | Serves: 1"
-                        newPrice="$10.50"
-                        oldPrice="$11.70"
-                    />
-                    <CustomCard
-                        image={burgerImg}
-                        altImg="burger-image"
-                        description="Vegetarian Burger"
-                        time="Time: 30 - 45 Minutes | Serves: 1"
-                        newPrice="$9.20"
-                        oldPrice="$10.50"
-                    />
-                    <CustomCard
-                        image={pizzaImg}
-                        altImg="pizza-image"
-                        description="Raspberry Stuffed French Toast"
-                        time="Time: 10 - 15 Minutes | Serves: 1"
-                        newPrice="$12.50"
-                        oldPrice="$13.20"
-                    />
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="explore overflow-hidden" id="explore">
+      <div className="container">
+        <h4 className="fw-bold text-center" data-aos="fade-right">
+          Explore Our Foods
+        </h4>
+        <p className="text-center my-4" data-aos="fade-left">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et
+          purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat
+          tellus. Far far away, behind the word mountains, far from the
+          countries Vokalia and Consonantia, there live the blind texts.
+          Separated they live in Bookmarksgrove.
+        </p>
+        <div
+          className="row justify-content-between"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
+          <CustomCard
+            image={vegSandwich}
+            altImg="vegetable-sandwich"
+            description="Rainbow Vegetable Sandwich"
+            time="15 - 20 Minutes | Serves: 1"
+            newPrice="$10.50"
+            oldPrice="$11.70"
+          />
+          <CustomCard
+            image={burgerImg}
+            altImg="burger-image"
+            description="Vegetarian Burger"
+            time="Time: 30 - 45 Minutes | Serves: 1"
+            newPrice="$9.20"
+            oldPrice="$10.50"
+          />
+          <CustomCard
+            image={pizzaImg}
+            altImg="pizza-image"
+            description="Raspberry Stuffed French Toast"
+            time="Time: 10 - 15 Minutes | Serves: 1"
+            newPrice="$12.50"
+            oldPrice="$13.20"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Explore;
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,44 +1,32 @@
-import CustomButton from './CustomButton';
+import CustomButton from "./CustomButton";
 
 const Subscribe = () => {
-    return (
-        <section className="subscribe py-5">
-            <div className="container">
-                <div className="row text-center">
-                    <h4 className="fs-1 fw-bold" data-aos="fade-left">
-                        Hurry up! Subscribe our newsletter and get 25% Off
-                    </h4>
-                    <p
-                        className="my-4"
-                        data-aos="fade-right"
-                        data-aos-delay="50"
-                    >
-                        Limited time offer for this month. No credit card
-                        required.
-                    </p>
-                    <form
-                        className="mt-3"
-                        data-aos="fade-up"
-                        data-aos-delay="100"
-                    >
-                        <input
-                            type="email"
-                            name="email"
-                            autoComplete="email"
-                            placeholder="Enter Your Email"
-                            className="col-8 col-lg-6 me-0 me-lg-3 px-4 py-2"
-                        />
-                        <button
-                            type="button"
-                            className="px-4 py-2 fw-bold col-8 col-lg-3"
-                        >
-                            Subscribe
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="subscribe py-5 overflow-hidden">
+      <div className="container">
+        <div className="row text-center">
+          <h4 className="fs-1 fw-bold" data-aos="fade-left">
+            Hurry up! Subscribe our newsletter and get 25% Off
+          </h4>
+          <p className="my-4" data-aos="fade-right" data-aos-delay="50">
+            Limited time offer for this month. No credit card required.
+          </p>
+          <form className="mt-3" data-aos="fade-up" data-aos-delay="100">
+            <input
+              type="email"
+              name="email"
+              autoComplete="email"
+              placeholder="Enter Your Email"
+              className="col-8 col-lg-6 me-0 me-lg-3 px-4 py-2"
+            />
+            <button type="button" className="px-4 py-2 fw-bold col-8 col-lg-3">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
